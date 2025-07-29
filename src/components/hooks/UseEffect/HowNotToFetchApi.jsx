c4e33e7575290054be2ec81fa160a3188620daf3
Devraj <devrajmandal5650@gmail.com>
2025-07-30 01:17:34 +0530

    Incremental updates

--- src/components/hooks/UseEffect/HowNotToFetchApi.jsx
+++ src/components/hooks/UseEffect/HowNotToFetchApi.jsx
@@ -3,16 +3,16 @@ import "./Pokemon.css";
 
 export const HowNotToFetchApi = () => {
   const [apiData, setApiData] = useState([]);
-  fetch("https://jsonplaceholder.typicode.com/posts")
+  fetch("https://dummy-json.mock.beeceptor.com/continents")
     .then((res) => res.json())
     .then((data) => setApiData(data))
     .catch((error) => console.log(error));
   return (
     <div className="container effect-container">
       <ul>
-        Data:
-        {apiData.map((curData) => {
-          return <li key={curData.id}>{curData.body}</li>;
+        data:
+        {apiData.map((curElem) => {
+          return <li key={curElem.id}>{curElem.name}</li>;
         })}
       </ul>
     </div>

9e3531a2d1cb508cea00fd1e842b13603dedf75f
Devraj <devrajmandal5650@gmail.com>
2025-08-07 14:55:58 +0530

    Incremental changes

--- src/components/hooks/UseRef/index.jsx
+++ src/components/hooks/UseRef/index.jsx
@@ -1,14 +1,20 @@
 import { useRef } from "react";
 import "./index.css";
 export const UseRef = () => {
+  // const username = document.getElementById("username");
+  // const password = document.getElementById("password");
+
   const username = useRef(null);
   const password = useRef(null);
-  const handleSubmit = (e) => {
+
+  // console.log(username);
+
+  const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log(username.current.value, password.current.value);
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleFormSubmit}>
       <input type="text" id="username" ref={username} />
       <br />
       <input type="text" id="password" ref={password} />
